1b38e9f9e0de40054aceb33a9a62b9268b03a624
shahules <shahules786@gmail.com>
2017-05-25 23:16:18 +0530

    Update weatherCelsius.py

--- weatherCelsius.py
+++ weatherCelsius.py
@@ -4,12 +4,21 @@
 
 
 #coder:Shahules786 
-import requests
-from bs4 import BeautifulSoup
-from math import floor as f
-from termcolor import colored
-
-res=requests.get("https://www.msn.com/en-us/weather")
+try:
+   import requests
+   from bs4 import BeautifulSoup
+   from math import floor as f
+   from termcolor import colored
+except ImportError:
+  print("import error")
+  
+  
+  
+try:
+   res=requests.get("https://www.msn.com/en-us/weather")
+except Exception as e:
+  print(colored("No Internet","blue"))
+  exit(0)
 bsobj=BeautifulSoup(res.text,"html.parser")
 ftemp=bsobj.find("span",{"class":"current"})
 temp=int(ftemp.get_text())
